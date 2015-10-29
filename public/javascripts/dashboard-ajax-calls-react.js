d55eddb52cbc02a9931e051ee2369514ad5e49c1
Shawn <SOtomo@digitalmoneycorp.com>
2015-10-29 13:17:13 -0700

    using ajax call in react now

--- public/javascripts/dashboard-ajax-calls-react.js
+++ public/javascripts/dashboard-ajax-calls-react.js
@@ -1,19 +1,19 @@
 var personas = null;
 
 //This will pull the social personas from the dash_summ  .json object only for now. Can be used to get professional ones as well
-$.ajax({
-	url: '.././json_files/personaSchema.json',
-	method: 'GET',
-	dataType: 'JSON',
-	success: function(response){
-		personas = response;
-        console.log(response);
-	},
-    error: function(response){
-        console.log('ERROR, personas not loaded');
+// $.ajax({
+// 	url: '.././json_files/personaSchema.json',
+// 	method: 'GET',
+// 	dataType: 'JSON',
+// 	success: function(response){
+// 		personas = response;
+//         console.log(response);
+// 	},
+//     error: function(response){
+//         console.log('ERROR, personas not loaded');
 
-    }
-});
+//     }
+// });
 
 //ajax call to pull in transactions data
 // $.ajax({
--- public/javascripts/individualCommView/individualCommView.jsx
+++ public/javascripts/individualCommView/individualCommView.jsx
@@ -1,77 +1,101 @@
+
+
+let CommPost = React.createClass({
+	render: function(){
+		return(
+			<div className="row">
+				<div className="col-sm-12 commPost">
+					<div className=" col-sm-12 commName">
+						<div className="col-sm-2 commProfileImage">
+							<a href="#"><img src="images/man2.jpg"></img></a>
+						</div>
+						<div className="col-sm-9 commPersonaPost">
+							<a href="#">Persona1234</a> <span>posted</span> Bangkok coconut curry bowls. I have to try making this for dinner tonight....
+							<br></br>
+							<a href="http://pinchofyum.com/bangkok-coconut-curry-noodle-bowls" target="blank"></a>
+						</div>
+						<div className="col-sm-1 col-sm-offset-1 commPostDate">
+							2 min ago
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-12 commPostControls">
+						<a href="#"><i className=" col-sm-1 col-sm-offset-1 fa fa-star"></i></a>
+						<div className="verticalLine"></div>
+						<div className="col-sm-2 commCommentBtn">
+							<div className="btn commentBtn">Comment</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+});
+
+let PostPanel = React.createClass({
+	render: function(){
+		return(
+			<div className="row">
+				<div className="col-sm-12 commTextField">
+					<div className="col-sm-12 commPostField">
+						<form action="">
+							<textarea placeholder="Post to all of your Communities" rows="6"></textarea>
+						</form>
+					</div>
+					<div className="col-sm-12 commPostBtn">
+						<div className="col-sm-12 btn">Post</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+});
+
+let CommHeading = React.createClass({
+	render: function(){
+		return(
+			<div className="row" id="commContainer">
+				<div className="col-sm-6 commControlBarLeft">
+					<div className=" col-sm-12 commProfileInfo">
+						<div className="col-sm-2 commImage">
+							<img src="images/cooking.jpg"></img>
+						</div>
+						<div className="col-sm-6 commName"><a href="#">For the Love of Cooking</a></div>
+						<div className="col-sm-3 commStatus">
+							<div className="btn btn-success">Request</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-6 commControlBarRight">
+					<div className="col-sm-12">
+						<div className="col-sm-7 col-sm-offset-2 commNavItems">
+							<ul>
+								<li className="pull-left"><a href="#">Calendar</a></li>
+								<li className="pull-right"><a href="#">Members</a></li>
+							</ul>
+						</div>
+						<div className="col-sm-3 commIconElements">
+							<a href="#"><i className="fa fa-star"></i></a>
+							<i className="fa fa-search"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+});
+
 var IndCommView = React.createClass({
 	render: function(){
 		return (
 			<div>
-				<div className="row" id="commContainer">
-					<div className="col-sm-6 commControlBarLeft">
-						<div className=" col-sm-12 commProfileInfo">
-							<div className="col-sm-2 commImage">
-								<img src="images/cooking.jpg"></img>
-							</div>
-							<div className="col-sm-6 commName"><a href="#">For the Love of Cooking</a></div>
-							<div className="col-sm-3 commStatus">
-								<div className="btn btn-success">Request</div>
-							</div>
-						</div>
-					</div>
-					<div className="col-sm-6 commControlBarRight">
-						<div className="col-sm-12">
-							<div className="col-sm-7 col-sm-offset-2 commNavItems">
-								<ul>
-									<li className="pull-left"><a href="#">Calendar</a></li>
-									<li className="pull-right"><a href="#">Members</a></li>
-								</ul>
-							</div>
-							<div className="col-sm-3 commIconElements">
-								<a href="#"><i className="fa fa-star"></i></a>
-								<i className="fa fa-search"></i>
-							</div>
-						</div>
-					</div>
-				</div>
-
+				<CommHeading />
+				<PostPanel />
+				<CommPost />
 				<div className="row">
-					<div className="col-sm-12 commTextField">
-						<div className="col-sm-12 commPostField">
-							<form action="">
-								<textarea placeholder="Post to all of your Communities" rows="6"></textarea>
-							</form>
-						</div>
-						<div className="col-sm-12 commPostBtn">
-							<div className="col-sm-12 btn">Post</div>
-						</div>
-					</div>
-				</div>
-
-				<div className="row">
-					<div className="col-sm-12 commFirstPost">
-						<div className=" col-sm-12 commFirstName">
-							<div className="col-sm-2 commProfileImage">
-								<a href="#"><img src="images/man2.jpg"></img></a>
-							</div>
-							<div className="col-sm-9 commPersonaPost">
-								<a href="#">Persona1234</a> <span>posted</span> Bangkok coconut curry bowls. I have to try making this for dinner tonight....
-								<br></br>
-								<a href="http://pinchofyum.com/bangkok-coconut-curry-noodle-bowls" target="blank"></a>
-							</div>
-							<div className="col-sm-1 col-sm-offset-1 commPostDate">
-								2 min ago
-							</div>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-sm-12 commPostControls">
-							<a href="#"><i className=" col-sm-1 col-sm-offset-1 fa fa-star"></i></a>
-							<div className="verticalLine"></div>
-							<div className="col-sm-2 commCommentBtn">
-								<div className="btn commentBtn">Comment</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-sm-12 commFirstPost">
-						<div className=" col-sm-12 commFirstName">
+					<div className="col-sm-12 commPost">
+						<div className=" col-sm-12 commName">
 							<div className="col-sm-2 commProfileImage">
 								<a href="#"><img src="images/arnold.jpg"></img></a>
 							</div>
