1733ad3b776ac8564e43595118cfdb1c543f3e58
U-DMC\jmaldonado <jmaldonado@MIG700688.dmc.local>
2015-11-12 08:19:23 -0800

    continued to make changes to left sidebar

--- public/javascripts/jsx/userDashboard/personaIndex/addpersona.jsx
+++ public/javascripts/jsx/userDashboard/personaIndex/addpersona.jsx
@@ -1,8 +1,15 @@
+
+
 //INPUT: NONE
 //OUTPUT: CREATION OF NEW PERSONA IN PERSONA INDEX
 var React = require('react');
+var CollapseContainer = require('./collapseContainer');
 
 var AddPersona = React.createClass({
+	clickHandler: function() {
+		console.log("calling call back" + this.props.isOpen);
+		this.props.clickHandlerCallback(this.props.isOpen);
+	},
 	render: function(){
 		var myClassName = 'col-sm-12 addPersona out';
 		if (!this.props.isOpen) {
@@ -10,7 +17,10 @@ var AddPersona = React.createClass({
 		}
 		return(
 			<div className={myClassName}>
-				<p>+<span className='panel-title'> Add New Persona</span></p>
+				<p><span className='panel-title'> Create New Persona
+					<CollapseContainer arrowDirection={this.props.arrowDirection} collapsePersona={this.clickHandler} />
+				</span>
+				</p>
 			</div>
 		)
 	}
--- public/javascripts/jsx/userDashboard/personaIndex/personaContainer.jsx
+++ public/javascripts/jsx/userDashboard/personaIndex/personaContainer.jsx
@@ -1,10 +1,12 @@
+
+
 //INPUT: NONE
 //OUTPUT: CREATION OF NEW PERSONA IN PERSONA INDEX
 //ACCORDION EFFECT WHEN USER CLICKS ON ARROW OR IMAGE
 var React = require('react');
 var AddPersona = require('./addpersona');
 var PersonaIndex = require('./personaindex');
-var CollapseContainer = require('./collapseContainer');
+
 
 var PersonaContainer = React.createClass({
 	getInitialState: function() {
@@ -13,25 +15,24 @@ var PersonaContainer = React.createClass({
 			arrowDirection: '<<'
 		}
 	},
-	clickHandler: function() {
-		 if(this.state.open) {
-		 	this.setState({
+	clickHandler: function(isOpen) {
+		if(isOpen) {
+			this.setState({
 				open: false,
 				arrowDirection: '>>'
 			});
-		 } else {
-		 	this.setState({
+		} else {
+			this.setState({
 				open: true,
 				arrowDirection: '<<'
 			});
-		 }
+		}
 	},
 	render: function(){
 		return(
 			<div className="personaContainer">
-				<AddPersona isOpen={this.state.open}/>
-				<CollapseContainer arrowDirection={this.state.arrowDirection} collapsePersona={this.clickHandler} />
-				<PersonaIndex isOpen={this.state.open} personas={this.props.personas} />
+				<AddPersona isOpen={this.state.open} clickHandlerCallback={this.clickHandler} arrowDirection={this.state.arrowDirection}/>
+				<PersonaIndex isOpen={this.state.open}  personas={this.props.personas} />
 			</div>
 		)
 	}
