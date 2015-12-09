53927272dd55e65716fc93b15eb6e9b2386f9d1c
U-DMC\jmaldonado <jmaldonado@MIG700688.dmc.local>
2015-12-08 16:43:30 -0800

    completed profile tab and inner tabs

--- public/javascripts/jsx/userDashboard/personaPicker/messageContainer.jsx
+++ public/javascripts/jsx/userDashboard/personaPicker/messageContainer.jsx
@@ -3,9 +3,17 @@
 var MessageContainer = React.createClass({
 	render: function(){
 		return(
-			<div className='bodyContent'>
-				<span> messages lorem ipsem for for {this.props.activePersona.persona_name}
-				</span>
+			<div className="col-sm-12 bodyContent">
+				<div className="col-sm-2 msgTitle">
+					<h4><i className="fa fa-envelope"></i>Messages</h4>
+				</div>
+				<div className="col-sm-9 communityNavTabs">
+					<ul className="nav nav-tabs" role="tablist">
+						<li role="presentation"><a href="#inbox" aria-controls="inbox" role="tab" data-toggle="tab">Inbox</a></li>
+						<li role="presentation"><a href="#outbox" aria-controls="outbox" role="tab" data-toggle="tab">Outbox</a></li>
+						<li role="presentation"><a href="#compose" aria-controls="compose" role="tab" data-toggle="tab">Compose</a></li>
+					</ul>
+				</div>	
 			</div>
 		)
 	}
