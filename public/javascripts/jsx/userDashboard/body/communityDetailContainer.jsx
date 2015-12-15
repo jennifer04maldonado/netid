9c7e61f3fa767a420ad79dd00e5f3b072ec07f9f
U-DMC\jmaldonado <jmaldonado@MIG700688.dmc.local>
2015-12-14 16:42:00 -0800

    completed one page profile page

--- public/javascripts/jsx/userDashboard/body/communityDetailContainer.jsx
+++ public/javascripts/jsx/userDashboard/body/communityDetailContainer.jsx
@@ -5,13 +5,21 @@ var CommunityDetailContainer = React.createClass({
 	render: function(){
 		return(
 			<div className="col-sm-12 bodyContent">
-				<a onClick={this.viewList} href="#back"> Back </a>
-
-				Community Detail
-				<h2> {this.props.activeCommunity.id}</h2>
-				<p>{this.props.activeCommunity.name}</p>
-				<p>{this.props.activeCommunity.description}</p>
-
+				<div className="col-sm-12 commDetailTitle">	
+					<h4><a onClick={this.viewList} href="#back"><i className="fa fa-arrow-left"></i>Back to Explore Communities</a></h4>
+				</div>	
+				<div className="col-sm-10 col-sm-offset-2 commDetailBody">
+					<img src={"/images/starwars.jpg"} className="col-sm-offset-2"/>
+					<div className="col-sm-offset-2 col-sm-6 commDetailText">	
+						<h3>{this.props.activeCommunity.name}</h3>
+						<p>{this.props.activeCommunity.id}</p>
+						<p>{this.props.activeCommunity.description}</p>
+						<h6>
+							<img src={"/images/friends.png"}/>
+								<a href="#">130 Members</a>
+						</h6>
+					</div>	
+				</div>	
 			</div>
 		)
 	}
--- public/javascripts/jsx/userDashboard/body/settingsContainer.jsx
+++ public/javascripts/jsx/userDashboard/body/settingsContainer.jsx
@@ -4,260 +4,77 @@ var SettingsContainer = React.createClass({
 	render: function(){
 		return(
 			<div className="col-sm-12 settingsCntnr">
-				<div className="col-sm-2 settingsTitle">
+				<div className="col-sm-12 settingsTitle">
 					<h4><i className="fa fa-user"></i>Profile</h4>
 				</div>
-				<div className="col-sm-9 communityNavTabs">
-					<ul className="nav nav-tabs" role="tablist">
-						<li role="presentation"><a href="#posts" aria-controls="posts" role="tab" data-toggle="tab">Posts</a></li>
-						<li role="presentation"><a href="#about" aria-controls="about" role="tab" data-toggle="tab">About</a></li>
-						<li role="presentation"><a href="#communities" aria-controls="communities" role="tab" data-toggle="tab">Communities</a></li>
-						<li role="presentation"><a href="#connections" aria-controls="connections" role="tab" data-toggle="tab">Connections</a></li>
-						<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
-
-					</ul>
-					<div className="tab-content col-sm-12">
-						<div role="tabpanel" className="tab-pane tabPosts fade" id="posts">
-							<h2>Posts</h2>
+				<div className="col-sm-8 col-sm-offset-2 profileDetailBody">
+					<img src={"/images/ein.jpeg"} className="col-sm-offset-2 profileImage"/>
+					<div className="col-sm-offset-2 col-sm-8 profileDetailText">	
+						<h3 className="row ">NatetheGreat
+							<a href="#"><i className="fa fa-pencil-square-o"></i></a>
+							<ul className="col-sm-9 col-sm-offset-1 profileScoreList">
+								<li className="masterScoreListItem">Master Score - 78</li>
+								<li>Social - 39</li>
+								<li>Professional - 81</li>
+							</ul>
+						</h3>
+						<p>In as name to here them deny wise this. As rapid woody my he me which. Men but they fail shew just wish next put. Led all visitor musical calling nor her. Within coming figure sex things are. Pretended concluded did repulsive education smallness yet yet described. Had country man his pressed shewing. No gate dare rose he. Eyes year if miss he as upon. </p>
+					</div>	
+		        </div>
+		        <div className="col-sm-8 col-sm-offset-2 profileInputField">
+					<div className="well">	
+						<form>
+							<input type="text" className="form-control" placeholder="Post something here" ></input>
+						</form>
+						<button className="btn">Post to your wall</button>
+					</div>	
+				</div>
+				<div className="row col-sm-8 col-sm-offset-2 media profilePostsElementText">
+					<div className="media-left">
+					    <a href="#">
+					      <img className="media-object" src={"/images/ein.jpeg"}/>
+					    </a>
+					</div>
+					<div className="media-body">
+					    <h4 className="media-heading">NatetheGreat</h4>
+					    <span className="postContentText">Spoke as as other again ye. Hard on to roof he drew. So sell side ye in mr evil. Longer waited mr of nature seemed. Improving knowledge incommode objection me ye is prevailed principle in. Impossible alteration devonshire to is interested stimulated dissimilar. To matter esteem polite.</span>
+						<br></br>
+						<span className="postTimeStamp">2 hours ago</span>
+					</div>
+				</div>	
+				<div className="row col-sm-8 col-sm-offset-2 media profilePostsElementPicture">
+					<div className="media-left">
+					    <a href="#">
+					      <img className="media-object" src={"/images/ein.jpeg"}/>
+					    </a>
+					</div>
+					<div className="media-body">
+					    <h4 className="media-heading">NatetheGreat</h4>
+					    <span className="postImageCaption">Had a great time last night making dinner with my family! Yum!</span>
+					    <img src={"/images/breakfast-pizza.jpg"}/>
+						<br></br>
+						<span className="postTimeStamp">10 days ago</span>
+					</div>
+				</div>	
+				<div className="row col-sm-8 col-sm-offset-2 media profilePostsElementPicture">
+					<div className="media-left">
+					    <a href="#">
+					      <img className="media-object" src={"/images/ein.jpeg"}/>
+					    </a>
+					</div>
+					<div className="media-body">
+					    <h4 className="media-heading">NatetheGreat</h4>
+					    <span className="postImageCaption">So funny...I love these videos</span>
+					    <div className="embed-responsive embed-responsive-16by9 profilePostsElementVideo">
+						    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/VJvRtkV5D3Y"></iframe>
 						</div>
-						<div role="tabpanel" className="tab-pane tabAbout fade" id="about">
-							<div className="col-sm-12 personaProfileAbout">
-		                    	<div className="row col-sm-10 addPersonaImage">
-									<div className="col-sm-6 col-sm-offset-4 editImageBtn">
-										<img src={"/images/ein.jpeg"}/>
-									</div>
-									<div className="col-sm-12 editImageUpload">
-										<h4 className="col-sm-4 col-sm-offset-4">NatetheGreat
-											<a href="#"><i className="fa fa-pencil-square-o"></i></a>
-										</h4>
-										<ul className="col-sm-4 col-sm-offset-4">
-											<li className="masterScoreListItem">Master Score - 78</li>
-											<li>Social - 39</li>
-											<li>Professional - 81</li>
-										</ul>
-										<h5 className="col-sm-11 col-sm-offset-1">Started his hearted any civilly. So me by marianne admitted speaking. Men bred fine call ask. Cease one miles truth day above seven. Suspicion sportsmen provision suffering mrs saw engrossed something. Snug soon he on plan in be dine some.</h5>
-									</div>
-								</div>
-							</div>	
-						</div>
-						<div role="tabpanel" className="tab-pane tabCommunities fade" id="communities">
-							<div className="col-sm-12 settingsCommunities">
-								<div className="panel panel-default">
-								  	<div className="panel-body">
-								    	<div className="media">
-											<div className="media-left">
-											    <a href="#">
-											      <img className="media-object" src={"/images/cooking.jpg"}/>
-											    </a>
-											</div>
-											<div className="media-body">
-											    <h5 className="media-heading">Cooking At Home</h5>
-											    <span>Spoke as as other again ye. Hard on to roof he drew. So sell side ye in mr evil. Longer waited mr of nature seemed. Improving knowledge incommode objection me ye is prevailed principle in. Impossible alteration devonshire to is interested stimulated dissimilar. To matter esteem polite.</span>
-												<h6>
-													<img src={"/images/friends.png"}/>
-													130 Members
-													<i className="fa fa-unlock-alt"></i>
-													Public
-												</h6>
-											</div>
-										</div>
-								  	</div>
-								  	<div className="panel-body">
-								    	<div className="media">
-											<div className="media-left">
-											    <a href="#">
-											      <img className="media-object" src={"/images/football-fanss.jpg"}/>
-											    </a>
-											</div>
-											<div className="media-body">
-											    <h5 className="media-heading">Football Fans</h5>
-											    <span>Spoke as as other again ye. Hard on to roof he drew. So sell side ye in mr evil. Longer waited mr of nature seemed. Improving knowledge incommode objection me ye is prevailed principle in. Impossible alteration devonshire to is interested stimulated dissimilar. To matter esteem polite.</span>
-												<h6>
-													<img src={"/images/friends.png"}/>
-													130 Members
-													<i className="fa fa-unlock-alt"></i>
-													Public
-												</h6>
-											</div>
-										</div>
-								  	</div>
-								  	<div className="panel-body">
-								    	<div className="media">
-											<div className="media-left">
-											    <a href="#">
-											      <img className="media-object" src={"/images/coders.jpg"}/>
-											    </a>
-											</div>
-											<div className="media-body">
-											    <h5 className="media-heading">Coders United</h5>
-											    <span>Spoke as as other again ye. Hard on to roof he drew. So sell side ye in mr evil. Longer waited mr of nature seemed. Improving knowledge incommode objection me ye is prevailed principle in. Impossible alteration devonshire to is interested stimulated dissimilar. To matter esteem polite.</span>
-												<h6>
-													<img src={"/images/friends.png"}/>
-													130 Members 
-													<i className="fa fa-lock"></i>
-													Private
-												</h6>
-											</div>
-										</div>
-								  	</div>
-								</div>
-							</div>	
-						</div> 
-						<div role="tabpanel" className="tab-pane tabConnections fade" id="connections">
-							<div className="col-sm-12 settingsConnections">
-			                	<div className="panel panel-default">
-								  	<div className="panel-body">
-								    	<div className="media">
-											<ul className="col-sm-12 profileConnectionsList">
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/dolph.png"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">MyNameIsCoolMan</h5>
-													   <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/jenn.jpeg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">JustinChang123</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/kim.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">PeterRamos998</h5>
-													  	<h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-											</ul>
-											<ul className="col-sm-12 profileConnectionsList">
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/man1.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">MyNameIsCoolMan</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/man2.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">JustinChang123</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/carrottop.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">PeterRamos998</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-											</ul>
-											<ul className="col-sm-12 profileConnectionsList">
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/person4.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">MyNameIsCoolMan</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/person3.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">JustinChang123</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/person1.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">PeterRamos998</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-											</ul>
-											<ul className="col-sm-12 profileConnectionsList">
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/girl3.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">MyNameIsCoolMan</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/girl2.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">JustinChang123</h5>
-													    <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-												<li>
-													<div className="media-left">
-													   	<a href="#">
-													        <img className="media-object settingsFriendImg" src={"/images/girl.jpg"}/>
-													    </a>
-													</div>
-													<div className="media-body settingsFriendName">
-													    <h5 className="media-heading">PeterRamos998</h5>
-													   <h6><i className="fa fa-times"></i>Remove/ Block</h6>
-													</div>
-												</li>
-											</ul>				
-										</div>
-								  	</div> 
-								</div> 
-							</div>	
-						</div>
-						<div role="tabpanel" className="tab-pane tabSettings fade" id="settings">
-							<h2>Settings</h2>
-						</div>
-					</div> 
-		        </div> 
+						<br></br>
+						<span className="postTimeStamp">10 days ago</span>
+					</div>
+				</div>
+				<div className="row col-sm-7 col-sm-offset-3 seeMoreBtn">
+					<button className="btn">See more posts</button>
+				</div>
 			</div> 
 		)
 	}
