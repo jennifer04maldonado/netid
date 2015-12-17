c0cc700bf9de8cdac69254253ab8a76ab30e4dda
U-DMC\jmaldonado <jmaldonado@MIG700688.dmc.local>
2015-12-17 11:08:49 -0800

    completed individual community page

---
+++ public/javascripts/jsx/userDashboard/common/commCalendarModal.jsx
@@ -0,0 +1,23 @@
+var commCalendarModal = React.createClass({	
+	render: function(){
+	    return (
+		<div id="commCalendarModal" className="modal fade" role="dialog">
+			<div className="modal-dialog">
+			    <div className="modal-content">
+				    <div className="modal-header">
+				        <button type="button" className="close" data-dismiss="modal">&times;</button>
+				        <h4 className="modal-title"> Calendar</h4>
+				    </div>
+				    <div className="modal-body">
+				       <img src={"/images/calendarTemp.png"}/>
+				       <p className="row"><a href="#"><i className="fa fa-plus"></i>Add Event</a></p>
+				    </div>
+			    </div>
+			</div>
+		</div>  
+	    );
+	}
+});	
+
+module.exports = commCalendarModal;
+     	
