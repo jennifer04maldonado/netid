f4037981cf98c7908fbdc654f9985842dde1d17f
scott <scottkimdmc@gmail.com>
2016-01-12 12:15:26 -0800

    changed label to net id

--- public/javascripts/jsx/signUp/loginApp.jsx
+++ public/javascripts/jsx/signUp/loginApp.jsx
@@ -15,14 +15,14 @@ var LoginApp = React.createClass({
     var self = this;
     event.preventDefault();
     try {
-        var username = event.target.username.value;      
-        var  password = event.target.passcode.value;        
+        var netId = event.target.netId.value;      
+        var password = event.target.passcode.value;        
         //TODO: ipfs switch
-        this.authenticateUser(username, password, function(result) {
+        this.authenticateUser(netId, password, function(result) {
             if (result) {
               window.location.href = "/userDashboard";
             } else {
-              self.setState({status: "error", message: "Invalid User or Password!"})
+              self.setState({status: "error", message: "Invalid Net Id or Password!"})
             }
         });
     } catch (err){
@@ -42,11 +42,11 @@ var LoginApp = React.createClass({
               <strong>{this.state.message}</strong>
             </div>  
             <div className="alert alert-info">
-              <strong> default user/passcode: test/test</strong>
+              <strong> default Net Id/passcode: test/test</strong>
             </div>  
 
             <form onSubmit={this.loginUser} action="/userDashboard" method="post">
-                <input className="form-control loginInputs" id='login_username' type="text" name="username" placeholder="Username"/>
+                <input className="form-control loginInputs" id='login_username' type="text" name="netId" placeholder="Net Id"/>
                 <input className="form-control loginInputs" id='login_passcode' type="text" name="passcode" placeholder="Passcode"/>
                 <input className="form-control loginInputs" id='login_auth_app' type="text" name="authApp" placeholder="Authenticator App Code"/>
                 <button type="submit" className="col-sm-12 btn loginButton">Log In</button>                
