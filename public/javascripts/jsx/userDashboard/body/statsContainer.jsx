4bda78ead5c2ba8ba0679af4a200790405ab790f
scott <scottkimdmc@gmail.com>
2016-01-14 16:39:26 -0800

    added stats panel to body

---
+++ public/javascripts/jsx/userDashboard/body/statsContainer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var StatsContainer = React.createClass({
+	render: function(){
+		return(
+			<div className="">
+				statistic panel
+			</div> 
+		)
+	}
+});
+
+module.exports = StatsContainer;
